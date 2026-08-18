848c60f45d5071b935581b6bb06c0b8c60107bca
Curt Sellmer <sellmerfud@gmail.com>
2026-08-17 20:34:33 -0500

    refactor: use commacd:: to prefix function names
    Prefix functions with commacd:: rather than _commacd_
    so _commacd_main becomes commacd::main, etc.

--- commacd.sh
+++ commacd.sh
@@ -36,18 +36,18 @@
 
 
 if [ -z "$BASH_VERSION" ]; then
-  _commacd_errout "commacd: is only supported for bash"
+  commacd::errout "commacd: is only supported for bash"
   return
 fi
 
-_commacd_version() {
+commacd::version() {
   (
     VERSION=2.0.0
     echo "commacd $VERSION"
   )
 }
 
-_commacd_usage() {
+commacd::usage() {
   local fwd_msgs=(     "  , <target>           -- cd to child directory whose name matches <target>"
                        "                          <target> can consist of multiple components separated by slashes: eg: s/m/scala."
                        "                          Each component will first be matched at the start of the directory names."
@@ -74,7 +74,7 @@ _commacd_usage() {
   esac
 
   {
-      _commacd_versionl
+      commacd::versionl
     printf "USAGE: [OPTIONS] [ARGS]\n"
     printf "\nOPTIONS:\n"
     printf "  -f                   -- Reverse the value of COMMACD_NOFUZZYFALLBACK\n"
@@ -87,7 +87,7 @@ _commacd_usage() {
 }
 
 
-_commacd_errout() {
+commacd::errout() {
   local fmt="$1"
   shift
   # shellcheck disable=2059  # fmt variable expands to the format string
@@ -99,9 +99,9 @@ _commacd_errout() {
 # was absolute.  Each other part will always begin with a forward slash.
 # The output of this function should be redirectoed to `readarray` to convert
 # the parts into an array as follows:
-#  ==> readarray -t myarray <<< "$(_commacd_split "$PWD")"
+#  ==> readarray -t myarray <<< "$(commacd::split "$PWD")"
 # This ensures that paths with spaces in the component part are handled correctly.
-_commacd_split() {
+commacd::split() {
   local str="$1" lead_slash=""
   if [[ "$str" =~ ^/ ]]; then
     lead_slash="/"
@@ -113,7 +113,7 @@ _commacd_split() {
 # First arg is string used between each joined part
 # The rest of the args are returned as a single string joined
 # together.
-_commacd_join() {
+commacd::join() {
   local IFS="$1"
   shift
   echo "$*"
@@ -128,7 +128,7 @@ _commacd_join() {
 #  nullglob (set) - patterns that match not files expand to null string (not the pattern itself)
 #  failglob (unset) - patterns that fail to match result in expansion error
 #  globstar - allow globs with /**/ for deep search
-_commacd_expand() (
+commacd::expand() (
   shopt -s nocaseglob extglob nullglob globstar
   shopt -u failglob
   # shellcheck disable=SC2206  # Do not quote $1 here to allow globbing
@@ -141,7 +141,7 @@ _commacd_expand() (
 )
 
 # Change the current directory
-_commacd_cd() {
+commacd::cd() {
   local dir="$1" display
   display="$(builtin cd "$dir" || return 1)"
   if [[ -z "$display" ]]; then
@@ -153,30 +153,30 @@ _commacd_cd() {
 
 # Called to change the current directory
 # Will call a user supplied function if defined otherwise
-# falls back to _commacd_change_dir()
-_commacd_change_directory() {
+# falls back to commacd::change_dir()
+commacd::change_directory() {
   local dir="$1"
   [[ -z "$dir" ]] && return  # Use cancelled a selection
 
   if [[ "$PWD" == "$dir" ]]; then
-      _commacd_errout "commacd: no matches found"
+      commacd::errout "commacd: no matches found"
       return 1
   elif [[ -n "$COMMACD_CD" ]]; then
     $COMMACD_CD "$dir"
   else
-    _commacd_cd "$dir"
+    commacd::cd "$dir"
   fi
 }
 
 # show match selection menu
-_commacd_choose_match() {
+commacd::choose_match() {
   local -a matches
   readarray -t matches < <(printf "%s\n" "$@" | sort)
   local i=${COMMACD_SEQSTART:-0}
   local num="${#matches[@]}"
   local width=${#num}
   for match in "${matches[@]}"; do
-    _commacd_errout "%*s  %s" "$width" "$((i++))" "$match"
+    commacd::errout "%*s  %s" "$width" "$((i++))" "$match"
   done
   local selection
   local threshold=$((11-${COMMACD_SEQSTART:-0}))
@@ -197,10 +197,10 @@ _commacd_choose_match() {
         echo -n "${matches[i]}"
         return
       else
-        _commacd_errout "Invalid selection! %s" "$selection"
+        commacd::errout "Invalid selection! %s" "$selection"
       fi
     else
-      _commacd_errout "Invalid selection! %s" "$selection"
+      commacd::errout "Invalid selection! %s" "$selection"
     fi
   done
 }
@@ -208,7 +208,7 @@ _commacd_choose_match() {
 # takes a path and returns the same path with glob (*)
 # at the end of each component to allow for prefix matching
 #  /aaa/bbb/ccc ==> /aaa*/bbb*/ccc*/
-_commacd_prefix_glob() {
+commacd::prefix_glob() {
   local path="${1%/}" head=""
   if [[ "$path" =~ ^/ ]]; then
     # Absolute path
@@ -229,7 +229,7 @@ _commacd_prefix_glob() {
 # takes a path and returns the same path with glob (*)
 # at the start and end of each component to allow for infix matching
 #  /aaa/bbb/ccc ==> /*aaa*/*bbb*/*ccc*/
-_commacd_infix_glob() {
+commacd::infix_glob() {
   local path="${1%/}" head=""
   if [[ "$path" =~ ^/ ]]; then
     # Absolute path
@@ -248,8 +248,8 @@ _commacd_infix_glob() {
   printf "%s*%s*/" "$head" "$path"
 }
 
-# Utility function used by _commacd_forward()
-_commacd_forward_by_prefix() {
+# Utility function used by commacd::forward()
+commacd::forward_by_prefix() {
   local matches target="$1"
 
   # Filter out all matches that reference $PWD
@@ -267,20 +267,20 @@ _commacd_forward_by_prefix() {
     fi
   }
 
-  readarray -t matches < <(_commacd_expand "$(_commacd_prefix_glob "$target")")
+  readarray -t matches < <(commacd::expand "$(commacd::prefix_glob "$target")")
   filter_pwd
   if [[ ${#matches[@]} == 0 && "$_commacd_nofuzzyfallback" != "on" ]]; then
-    readarray -t matches < <(_commacd_expand "$(_commacd_infix_glob "$target")")
+    readarray -t matches < <(commacd::expand "$(commacd::infix_glob "$target")")
     filter_pwd
   fi
 
   # If no mathces found and the target is a relative path,
   # then try a deep search.
   if [[ ${#matches[@]} == 0 && "$_commacd_nodeepfallback" != "on" && ! "$target" =~ ^/ ]]; then
-    readarray -t matches < <(_commacd_expand "$(_commacd_prefix_glob "$target" deep)")
+    readarray -t matches < <(commacd::expand "$(commacd::prefix_glob "$target" deep)")
     filter_pwd
     if [[ ${#matches[@]} == 0 && "$_commacd_nofuzzyfallback" != "on" ]]; then
-      readarray -t matches < <(_commacd_expand "**/$(_commacd_infix_glob "$target" deep)")
+      readarray -t matches < <(commacd::expand "**/$(commacd::infix_glob "$target" deep)")
       filter_pwd
     fi
   fi
@@ -292,11 +292,11 @@ _commacd_forward_by_prefix() {
 
 
 # jump forward (`,`)
-_commacd_forward() {
+commacd::forward() {
   local matches dir
-  [[ -z "$1" ]] && { _commacd_usage "," ; return 1 ; }
+  [[ -z "$1" ]] && { commacd::usage "," ; return 1 ; }
 
-  readarray -t matches < <(_commacd_forward_by_prefix "$@")
+  readarray -t matches < <(commacd::forward_by_prefix "$@")
   if [[ "$COMMACD_NOTTY" == "on" ]]; then
     printf "%s\n" "${matches[@]}"
     return
@@ -304,24 +304,24 @@ _commacd_forward() {
 
   case ${#matches[@]} in
     0)
-      _commacd_errout "No match for '%s'" "$1"
+      commacd::errout "No match for '%s'" "$1"
       return 1
       ;;
     1)
-      _commacd_change_directory "${matches[0]}"
+      commacd::change_directory "${matches[0]}"
       ;;
     *)
       # https://github.com/shyiko/commacd/issues/12
       # trap 'trap - SIGINT; stty '"$(stty -g)" SIGINT
 
-      dir="$(_commacd_choose_match "${matches[@]}")"
+      dir="$(commacd::choose_match "${matches[@]}")"
       # make sure trap is removed regardless of whether read -e ... was
       # interrupted or not
       # trap - SIGINT
       if [[ -z "$dir" ]]; then
         return 0
       else
-        _commacd_change_directory "$dir"
+        commacd::change_directory "$dir"
       fi
       ;;
   esac
@@ -329,7 +329,7 @@ _commacd_forward() {
 
 # See if the subdirectory listed in the COMMACD_MARKERS
 # variable exists in the given directory
-_commacd_marked() {
+commacd::marked() {
   local dir markers
   dir="${1%/}"
   readarray -t markers < <(echo "${COMMACD_MARKER:-.git/ .hg/ .svn/}" | tr -s ' \t,:' '\n')
@@ -343,9 +343,9 @@ _commacd_marked() {
 
 # search backward for a directory containing a marker (`,,`)
 # ie. the root of a project
-_commacd_backward_projcect_root() {
+commacd::backward_projcect_root() {
   local dir="${PWD%/*}"
-  while [[ -n "$dir" ]] && ! _commacd_marked "$dir"; do
+  while [[ -n "$dir" ]] && ! commacd::marked "$dir"; do
     dir="${dir%/*}"
   done
 
@@ -353,15 +353,15 @@ _commacd_backward_projcect_root() {
     printf "%s\n" "$dir"
     return
   elif [[ -z "$dir" ]]; then
-    _commacd_errout "No project root found"
+    commacd::errout "No project root found"
     return 1
   else
-    _commacd_change_directory "$dir"
+    commacd::change_directory "$dir"
   fi
 }
 
 # search backward for the directory whose name begins with $1 (`,, $1`)
-_commacd_backward_by_prefix() {
+commacd::backward_by_prefix() {
   local parts num_parts idx dir target
   target="${1}"
   dir=""
@@ -370,12 +370,12 @@ _commacd_backward_by_prefix() {
   if [[ "${target:0:1}" == "/" ]]; then
     [[ -d "$target" ]] && dir="$target"
   else
-    readarray -t parts < <(_commacd_split "$PWD")
+    readarray -t parts < <(commacd::split "$PWD")
     num_parts=${#parts[@]}
     if ((num_parts > 1)); then
       for ((idx = num_parts - 2; idx >= 0; --idx)); do
         if [[ "${parts[idx],,}" == /${target,,}* ]]; then
-          dir="$(_commacd_join '' "${parts[@]:0:idx+1}")"
+          dir="$(commacd::join '' "${parts[@]:0:idx+1}")"
           break
         fi
       done
@@ -383,7 +383,7 @@ _commacd_backward_by_prefix() {
       if [[ -z "$dir" && "$_commacd_nofuzzyfallback" != "on" ]]; then
         for ((idx = num_parts - 2; idx >= 0; --idx)); do
           if [[ "$_commacd_nofuzzyfallback" != "on" && "${parts[idx],,}" == /*${target,,}* ]]; then
-            dir="$(_commacd_join '' "${parts[@]:0:idx+1}")"
+            dir="$(commacd::join '' "${parts[@]:0:idx+1}")"
             break
           fi
         done
@@ -395,21 +395,21 @@ _commacd_backward_by_prefix() {
     printf "%s\n" "$dir"
     return
   elif [[ -z "$dir" ]]; then
-    _commacd_errout "No match for '%s'" "$target"
+    commacd::errout "No match for '%s'" "$target"
     return 1
   else
-    _commacd_change_directory "$dir"
+    commacd::change_directory "$dir"
   fi
 }
 
 # replace $1 with $2 in $PWD (`,, $1 $2`)
-_commacd_backward_substitute() {
+commacd::backward_substitute() {
   # echo -n "${PWD/$1/$2}"
   local cwd_parts num_parts idx dir head_parts tail_parts
   local head_matches matches target
   local target_prefix="$1" repl_prefix="$2"
 
-  readarray -t cwd_parts < <(_commacd_split "$PWD")
+  readarray -t cwd_parts < <(commacd::split "$PWD")
   num_parts="${#cwd_parts[@]}"
   # Find right most part of the workding directory path
   # that starts with the target prefix
@@ -426,18 +426,18 @@ _commacd_backward_substitute() {
   fi
 
   if [[ $idx == -1 ]]; then
-    _commacd_errout "'%s' cannot be matched in the working directory path!" "$target_prefix"
+    commacd::errout "'%s' cannot be matched in the working directory path!" "$target_prefix"
     return 1
   else
     target="${cwd_parts[idx]}"  # The full target for error reporting
     # The head of the cwd path preceeding the target with
     # with the replacment prefix and a glob appended for lookup
     head_parts=("${cwd_parts[@]:0:idx}" "/$repl_prefix*")
-    head="$(_commacd_join '' "${head_parts[@]}")"
-    readarray -t head_matches < <(_commacd_expand "$head")
+    head="$(commacd::join '' "${head_parts[@]}")"
+    readarray -t head_matches < <(commacd::expand "$head")
     # The tail is everything following the replaced path part
     tail_parts=("${cwd_parts[@]:idx+1}")
-    tail="$(_commacd_join '' "${tail_parts[@]}")"
+    tail="$(commacd::join '' "${tail_parts[@]}")"
     # Find all directories that exist when we append the tail
     # too one of the head matches.
     final_matches=()
@@ -450,11 +450,11 @@ _commacd_backward_substitute() {
 
     if [[ ${#final_matches[@]} == 0 && "$_commacd_nofuzzyfallback" != "on" ]]; then
       head_parts=("${cwd_parts[@]:0:idx}" "/*$repl_prefix*")
-      head="$(_commacd_join '' "${head_parts[@]}")"
-      readarray -t head_matches < <(_commacd_expand "$head")
+      head="$(commacd::join '' "${head_parts[@]}")"
+      readarray -t head_matches < <(commacd::expand "$head")
       # The tail is everything following the replaced path part
       tail_parts=("${cwd_parts[@]:idx+1}")
-      tail="$(_commacd_join '' "${tail_parts[@]}")"
+      tail="$(commacd::join '' "${tail_parts[@]}")"
       # Find all directories that exist when we append the tail
       # too one of the head matches.
       final_matches=()
@@ -468,40 +468,40 @@ _commacd_backward_substitute() {
 
     case "${#final_matches[@]}" in
       0)
-        _commacd_errout "'%s' cannot be matched in same location as '%s'" "$repl_prefix" "${target:1}"
+        commacd::errout "'%s' cannot be matched in same location as '%s'" "$repl_prefix" "${target:1}"
         return 1
         ;;
       1)
-        _commacd_change_directory "${final_matches[0]}"
+        commacd::change_directory "${final_matches[0]}"
         ;;
       *)
-        dir="$(_commacd_choose_match "${final_matches[@]}")"
+        dir="$(commacd::choose_match "${final_matches[@]}")"
         if [[ -z "$dir" ]]; then
           return 0
         else
-          _commacd_change_directory "$dir"
+          commacd::change_directory "$dir"
         fi
     esac
   fi
 }
 
 # choose `,,` strategy based on a number of arguments
-_commacd_backward() {
+commacd::backward() {
   # when called for completion without args, we get an empty arg
   [[ $# == 1 && -z "$1" ]] && shift
   case $# in
-    0) _commacd_backward_projcect_root ;;
-    1) _commacd_backward_by_prefix "$1" ;;
-    2) _commacd_backward_substitute "$@" ;;
+    0) commacd::backward_projcect_root ;;
+    1) commacd::backward_by_prefix "$1" ;;
+    2) commacd::backward_substitute "$@" ;;
     *)
-      _commacd_errout
-      _commacd_usage ",,"
+      commacd::errout
+      commacd::usage ",,"
       return 1
       ;;
   esac
 }
 
-_commacd_backward_forward_by_prefix() {
+commacd::backward_forward_by_prefix() {
   local dir path matches
 
   # Filter out all matches that reference $PWD
@@ -525,19 +525,19 @@ _commacd_backward_forward_by_prefix() {
   dir="$PWD"
   while [[ -n "$dir" ]]; do
     dir="${dir%/*}"
-    readarray -t matches < <(_commacd_expand "$dir/$(_commacd_prefix_glob "$1")")
+    readarray -t matches < <(commacd::expand "$dir/$(commacd::prefix_glob "$1")")
     filter_pwd
     if [[ ${#matches[@]} == 0 && "$_commacd_nofuzzyfallback" != "on" ]]; then
-      readarray -t matches < <(_commacd_expand "$dir/$(_commacd_infix_glob "$1")")
+      readarray -t matches < <(commacd::expand "$dir/$(commacd::infix_glob "$1")")
       filter_pwd
     fi
 
     # If no mathces found then try a deep search.
     if [[ ${#matches[@]} == 0 && "$_commacd_nodeepfallback" != "on" ]]; then
-      readarray -t matches < <(_commacd_expand "$dir/$(_commacd_prefix_glob "$1" deep)")
+      readarray -t matches < <(commacd::expand "$dir/$(commacd::prefix_glob "$1" deep)")
       filter_pwd
       if [[ ${#matches[@]} == 0 && "$_commacd_nofuzzyfallback" != "on" ]]; then
-        readarray -t matches < <(_commacd_expand "$dir/**/$(_commacd_infix_glob "$1" deep)")
+        readarray -t matches < <(commacd::expand "$dir/**/$(commacd::infix_glob "$1" deep)")
       filter_pwd
       fi
     fi
@@ -551,12 +551,12 @@ _commacd_backward_forward_by_prefix() {
 }
 
 # combine backtracking with `, $1` (`,,, $1`)
-_commacd_backward_forward() {
-  [[ -z "$1" ]] && { _commacd_usage ",,," ; return 1 ; }
+commacd::backward_forward() {
+  [[ -z "$1" ]] && { commacd::usage ",,," ; return 1 ; }
 
   local IFS=$'\n'
   local candidates dir
-  readarray -t candidates < <(_commacd_backward_forward_by_prefix "$1")
+  readarray -t candidates < <(commacd::backward_forward_by_prefix "$1")
   if [[ "$COMMACD_NOTTY" == "on" ]]; then
     printf "%s\n" "${candidates[@]}"
     return
@@ -571,15 +571,15 @@ _commacd_backward_forward() {
       dir="${candidates[0]}"
       ;;
     *)
-      dir="$(_commacd_choose_match "${candidates[@]}")"
+      dir="$(commacd::choose_match "${candidates[@]}")"
       [[ -z "$dir" ]] && return  # user cancelled
       ;;
   esac
 
-  _commacd_change_directory "$dir"
+  commacd::change_directory "$dir"
 }
 
-_commacd_completion() {
+commacd::completion() {
   local pattern=${COMP_WORDS[COMP_CWORD]} IFS=$'\n'
   if [[ $COMP_CWORD == 1 && "$pattern" =~ ^- ]]; then
     compgen -V COMPREPLY -W $'-f\n-d\n-h\n-v\n--help\n--version' -- "$pattern"
@@ -598,22 +598,22 @@ _commacd_completion() {
   fi
 }
 
-_commacd_forward_completion() {
-  _commacd_completion _commacd_forward
+commacd::forward_completion() {
+  commacd::completion commacd::forward
 }
 
-_commacd_backward_completion() {
+commacd::backward_completion() {
   if [[ ${#COMP_WORDS[@]} -le 2 ]]; then
-    _commacd_completion _commacd_backward
+    commacd::completion commacd::backward
   fi
 }
 
-_commacd_backward_forward_completion() {
-  _commacd_completion _commacd_backward_forward
+commacd::backward_forward_completion() {
+  commacd::completion commacd::backward_forward
 }
 
 
-_commacd_main() {
+commacd::main() {
   local action="$1"
   shift
   local args=("$@")
@@ -627,12 +627,12 @@ _commacd_main() {
   for arg in "${args[@]}"; do
     case "$arg" in
       ( -v | --version )
-        _commacd_version
+        commacd::version
         $restore_shopt
         return 0
       ;;
       ( -h | --help )
-      _commacd_usage
+      commacd::usage
       $restore_shopt
       return 0
       ;;
@@ -662,7 +662,7 @@ _commacd_main() {
             fi
             ;;
           ( * )
-            _commacd_errout "Invalid option: '%s'\n" "-$opt"
+            commacd::errout "Invalid option: '%s'\n" "-$opt"
             $restore_shopt
             return 1
             ;;
@@ -683,12 +683,12 @@ _commacd_main() {
   return "$result"
 }
 
-alias ,='_commacd_main _commacd_forward'
-alias ,,='_commacd_main _commacd_backward'
-alias ,,,='_commacd_main _commacd_backward_forward'
+alias ,='commacd::main commacd::forward'
+alias ,,='commacd::main commacd::backward'
+alias ,,,='commacd::main commacd::backward_forward'
 
 if [ -n "$BASH_VERSION" ]; then
-  complete -o filenames -F _commacd_forward_completion ,
-  complete -o filenames -F _commacd_backward_completion ,,
-  complete -o filenames -F _commacd_backward_forward_completion ,,,
+  complete -o filenames -F commacd::forward_completion ,
+  complete -o filenames -F commacd::backward_completion ,,
+  complete -o filenames -F commacd::backward_forward_completion ,,,
 fi
